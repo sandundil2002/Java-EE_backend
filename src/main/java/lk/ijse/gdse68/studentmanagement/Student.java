2aeb6b0faf5806a087e9350cd6c65e2d3ad3672b
T.H. Sandun Dilshan <Sandundil2002@gmail.com>
2024-07-07 11:27:14 +0530

    student crud method override.

--- src/main/java/lk/ijse/gdse68/studentmanagement/Student.java
+++ src/main/java/lk/ijse/gdse68/studentmanagement/Student.java
@@ -1,7 +1,30 @@
 package lk.ijse.gdse68.studentmanagement;
 
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
+@WebServlet(urlPatterns = "/student")
 public class Student extends HttpServlet {
 
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) {
+        //Todo:save student
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
+        //Todo:search student
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) {
+        //Todo:update student
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) {
+        //Todo:delete student
+    }
 }
